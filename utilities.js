05da2db6373741762e69440518c902f2a3f3d381
Gianmarco Rengucci <rengucci.gianmarco@gmail.com>
2019-04-28 18:10:52 +0200

    [WIP]: Improve JS Methods, create dynamic product page
    Signed-off-by: Gianmarco Rengucci <rengucci.gianmarco@gmail.com>

--- utilities.js
+++ utilities.js
@@ -1,9 +1,8 @@
-/* Hide register if user is logged in, show profile and cart instead */
+// Hide navbar items if user is not logged in
 function navbarhider() {
     var userid = sessionStorage.getItem('userid');
     if (userid != null) {
-        console.log("acquired userid with value: ");
-        console.log(userid);
+        console.log("Acquired userid with value: " +userid);
         document.getElementById("shoppingcart").style.display = "block";
         document.getElementById("profile").style.display = "block";
     } else {
@@ -11,6 +10,7 @@ function navbarhider() {
     }
 }
 
+// Dynamically build user profile based on userid stored in sessionStorage
 function userinfo(){
     var userid = sessionStorage.getItem('userid');
     var usertype = sessionStorage.getItem('usertype');
@@ -39,13 +39,7 @@ function userinfo(){
     console.log("Profile dynamically loaded.");
 }
 
- // Run this on profile.html to avoin onload race conditions. TODO: Find a fix
- function profilepage(){
-    navbarhider();
-    userinfo();
-}
-
-/* Logout, clears all data */
+// Logout, clears all data
 function logout(){
     if (confirm("Sei sicuro di voler uscire?")){
         sessionStorage.clear(); // DO NOT CLEAR LOCALSTORAGE, or you will delete newly registered users
@@ -55,6 +49,7 @@ function logout(){
     }
 }
 
+// Delete account from localstorage
 function deleteaccount(){
     if (confirm("Sei sicuro di voler eliminare il tuo account?")){
         var userid = sessionStorage.getItem('userid');
@@ -72,4 +67,67 @@ function deleteaccount(){
         sessionStorage.clear();
         document.location.href="index.html";   
     }
+}
+
+// Dynamically build item page based on URL
+function productinfo(productid){
+    var itemlist = JSON.parse(localStorage.getItem("itemlist"));
+    var userlist = JSON.parse(localStorage.getItem("users"));
+    for(var i=0;i<itemlist.length;i++){
+        if (itemlist[i].ID == productid){
+            var itemname = itemlist[i].Nome;
+            var itemdesc = itemlist[i].Descrizione;
+            var itemprice =itemlist[i].Prezzo;
+            var itemimg = itemlist[i].Immagine;
+            var itemleft = itemlist[i].Quantita;
+            var itemshipment = itemlist[i].Spedizione;
+            var itemseller = itemlist[i].VendorID;
+            document.getElementById("itemname").innerHTML = itemname;
+            document.getElementById("itemdesc").innerHTML = itemdesc;
+            document.getElementById("itemprice").innerHTML = itemprice;
+            document.getElementById("itemimg").src = itemimg;
+            document.getElementById("itemleft").innerHTML = "Rimasti in magazzino: " +itemleft;
+            document.getElementById("itemshipment").innerHTML = "Metodo di spedizione: " +itemshipment;
+            for(var i=0;i<userlist[0].Venditori.length;i++){
+                if (userlist[0].Venditori[i].ID == itemseller){
+                    var itemseller = userlist[0].Venditori[i].attività;
+                    document.getElementById("itemseller").innerHTML= "Venduto da: " +itemseller;
+                  }
+            }
+        }
+    }
+    console.log("Product info dynamically loaded.");
+}
+
+// Load Product info
+function productloader(){
+    navbarhider();
+    // Use URLSearchParams to retrieve the queryID
+    const urlParams = new URLSearchParams(window.location.search);
+    const itemid = urlParams.get('itemid');
+    console.log("Acquired itemid with value: " +itemid)
+    productinfo(itemid);
+}
+
+// Build userlist and itemlist when index is loaded
+function indexloader(){
+    navbarhider();
+    if(localStorage.getItem("itemlist")==null){
+        //If itemlist is already present, don't override it
+        localStorage.setItem("itemlist", JSON.stringify(prodotti));
+        console.log("itemlist succesfully loaded.");
+    }
+    if(localStorage.getItem("users")==null){
+        //Initialize JSON into localstorage.
+        //Check if localstorage already exists, otherwise we may write deleted users on top of it.
+        localStorage.setItem("users", JSON.stringify(users));
+        console.log("userlist succesfully loaded.");
+  } 
+
+}
+
+// Run this on profile.html to avoin onload race conditions. TODO: Find a fix
+function profilepage(){
+    navbarhider();
+    userinfo();
 }
